b76ff2891fe28d3355bf06dd8057215fe332107b
sh2131 <sharo21311@gmail.com>
2025-05-10 00:37:14 +0300

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from django.shortcuts import render
 
 
+from django.template import loader
+from django.http import HttpResponse
 
 def home(request):
-   templates = loader.get_template("home.html")
+    templates = loader.get_template("home.html")
     return HttpResponse(templates.render())
+
 
 
 
