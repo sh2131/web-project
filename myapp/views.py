2c817fe99bd9251cc62fce0441fb39735fa84e34
sh2131 <sharo21311@gmail.com>
2025-05-10 00:39:55 +0300

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
-
-from django.template import loader
-from django.http import HttpResponse
-
 def home(request):
-    templates = loader.get_template("home.html")
-    return HttpResponse(templates.render())
+    return render(request, "home.html")
 
+def about(request):
+    return render(request, "about.html")
 
+def contact(request):
+    return render(request, "contact.html")
 
+def services(request):
+    return render(request, "services.html")
 
