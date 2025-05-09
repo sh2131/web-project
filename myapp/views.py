cc51a57614823bf46451538c4280f62e715d1aa5
sh2131 <sharo21311@gmail.com>
2025-05-10 00:03:11 +0300

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+
+
 def home(request):
-    return render(request, "myapp/index.html")
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def services(request):
+    return render(request, "services.html")
+
+
+
